a3b81125a6e3949d5b548dc1f5cf36d8da87a14f
tanlm <luuminhtan046@gmail.com>
2022-07-12 11:45:29 +0700

    Fix deploy SupplyChainManagementProgram

--- deploy/05_deploy_supply_chain_management_program.js
+++ deploy/05_deploy_supply_chain_management_program.js
@@ -1,13 +1,21 @@
 const { deployProxy } = require('../utils/deployHelper');
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { get } = deployments;
+  const { get, execute } = deployments;
+  const { deployer } = await getNamedAccounts();
   const registry = await get('Registry');
 
   //deploy SupplyChainManagementProgram
-  const securitizationManagerProxy = await deployProxy({ getNamedAccounts, deployments }, 'SupplyChainManagementProgram', [
+  const supplyChainManagementProgram = await deployProxy({ getNamedAccounts, deployments }, 'SupplyChainManagementProgram', [
     registry.address
   ]);
+  await execute(
+    'SupplyChainManagementProgram',
+    { from: deployer, log: true },
+    'grantRole',
+    '0xda2b0f370bd2974923a71e73c465a6368d3708f6b738cc46b9a1ac650e1de010',
+    deployer
+  );
 };
 
 module.exports.dependencies = ['registry'];
