735511106dfc72fa5c7abd69d38201fbc1f53038
tanlm <luuminhtan046@gmail.com>
2023-08-12 17:37:53 +0700

    Use rate scaling factor 10^4

--- contracts/protocol/pool/SecuritizationPoolValueService.sol
+++ contracts/protocol/pool/SecuritizationPoolValueService.sol
@@ -19,7 +19,7 @@ contract SecuritizationPoolValueService is
 {
     using ConfigHelper for Registry;
 
-    uint256 public constant RATE_SCALING_FACTOR = 10 ** 2;
+    uint256 public constant RATE_SCALING_FACTOR = 10 ** 4;
 
     function getPresentValueWithNAVCalculation(
         address poolAddress,
@@ -349,9 +349,8 @@ contract SecuritizationPoolValueService is
     function getBeginningSeniorAsset(address poolAddress) external view returns (uint256) {
         ISecuritizationPool securitizationPool = ISecuritizationPool(poolAddress);
         require(address(securitizationPool) != address(0), 'Pool was not deployed');
-        uint256 rateJunior = securitizationPool.minFirstLossCushion() / 10000;
-        require(rateJunior <= RATE_SCALING_FACTOR, 'securitizationPool.minFirstLossCushion greater 100');
-        uint256 rateSenior = RATE_SCALING_FACTOR - rateJunior;
+        uint256 rateJunior = securitizationPool.minFirstLossCushion();
+        uint256 rateSenior = 100 * RATE_SCALING_FACTOR - rateJunior;
         uint256 poolValue = this.getPoolValue(poolAddress);
         return (poolValue * rateSenior) / RATE_SCALING_FACTOR;
     }
@@ -469,9 +468,9 @@ contract SecuritizationPoolValueService is
 
     function getJuniorRatio(address poolAddress) external view returns (uint256) {
         uint256 rateSenior = this.getSeniorRatio(poolAddress);
-        require(rateSenior < RATE_SCALING_FACTOR, 'securitizationPool.minFirstLossCushion >100');
+        require(rateSenior < 100*RATE_SCALING_FACTOR, 'securitizationPool.rateSenior >100');
 
-        return RATE_SCALING_FACTOR - rateSenior;
+        return 100*RATE_SCALING_FACTOR - rateSenior;
     }
 
     function getSeniorRatio(address poolAddress) external view returns (uint256) {
