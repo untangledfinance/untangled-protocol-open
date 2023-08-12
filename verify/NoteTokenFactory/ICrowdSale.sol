d4e517a9d08465f9ff24c47476df5101f56182f8
tanlm <luuminhtan046@gmail.com>
2023-08-12 16:27:38 +0700

    Remove temp dir

--- verify/NoteTokenFactory/ICrowdSale.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-abstract contract ICrowdSale {
-    mapping(address => uint256) public currencyRaisedByInvestor;
-
-    uint256 public currencyRaised;
-}
