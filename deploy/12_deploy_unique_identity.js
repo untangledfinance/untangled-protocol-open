fc38fa79582b3949bfe4ed8864dc6b80da332dbd
tanlm <luuminhtan046@gmail.com>
2023-08-17 01:24:36 +0700

    UniqueIdentity: set support UUID type

--- deploy/12_deploy_unique_identity.js
+++ deploy/12_deploy_unique_identity.js
@@ -1,6 +1,7 @@
 const { deployProxy } = require('../utils/deployHelper');
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { execute } = deployments;
   const { deployer } = await getNamedAccounts();
 
   //deploy UniqueIdentity
@@ -8,6 +9,15 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const uniqueIdentityProxy = await deployProxy({ getNamedAccounts, deployments }, 'UniqueIdentity', [
     deployer, ''
   ]);
+  if (uniqueIdentityProxy.newlyDeployed) {
+    await execute(
+      'UniqueIdentity',
+      { from: deployer, log: true },
+      'setSupportedUIDTypes',
+      [0, 1, 2],
+      [true, true, true]
+    );
+  }
 };
 
 module.exports.tags = ['unique_identity'];
