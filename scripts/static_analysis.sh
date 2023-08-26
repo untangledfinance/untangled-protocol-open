2930227960589d65431be33cc3ca92299af55886
Tony Fung <tonybka@gmail.com>
2023-08-26 23:16:57 +0700

    Remove creating analysis result folder

--- scripts/static_analysis.sh
+++ scripts/static_analysis.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-OUTPUT_PATH="./analysis"
 IMAGE_NAME="slither-analyzer-local"
 
 if ! docker images | awk '{print $1}' | grep -q "^$IMAGE_NAME$"; then
@@ -10,9 +9,6 @@ if ! docker images | awk '{print $1}' | grep -q "^$IMAGE_NAME$"; then
 else
     echo "Image $IMAGE_NAME is already exists, ignore building image."
 fi
-
-# Create the output directory if it doesn't exist
-mkdir -p "$OUTPUT_PATH"
 
 # Run Docker with cleanup option
 docker run -it --rm $IMAGE_NAME .
