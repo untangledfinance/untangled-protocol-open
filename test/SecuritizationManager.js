490926d42ce45efc7e4610753d2ca7eb237b20ce
tanlm <luuminhtan046@gmail.com>
2023-08-28 21:22:45 +0700

    Update test SecuritizationManager

--- test/SecuritizationManager.js
+++ test/SecuritizationManager.js
@@ -7,7 +7,7 @@ const { expect } = require('./shared/expect.js');
 
 describe('SecuritizationManager', () => {
   let setupTest;
-  let tokenA;
+  let stableCoin;
   let securitizationManagerContract
 
   // Wallets
@@ -18,8 +18,8 @@ describe('SecuritizationManager', () => {
       async ({ deployments, getNamedAccounts, ethers }, options) => {
         await deployments.fixture(); // ensure you start from a fresh deployments
         const tokenFactory = await ethers.getContractFactory('TestERC20');
-        const tokenA = (await tokenFactory.deploy('cUSD', 'cUSD', BigNumber.from(2).pow(255)));
-        await tokenA.transfer(lenderSigner.address, BigNumber.from(1000).pow(18)) // Lender has 1000$
+        const stableCoin = (await tokenFactory.deploy('cUSD', 'cUSD', BigNumber.from(2).pow(255)));
+        await stableCoin.transfer(lenderSigner.address, BigNumber.from(1000).pow(18)) // Lender has 1000$
         const { get } = deployments;
         securitizationManagerContract = await ethers.getContractAt(
           'SecuritizationManager',
@@ -27,27 +27,14 @@ describe('SecuritizationManager', () => {
         );
 
         return {
-          tokenA: tokenA,
+          stableCoin: stableCoin,
         };
       },
     );
 
   });
   beforeEach('deploy fixture', async () => {
-    ({ tokenA } = await setupTest());
-  });
-  it('Should create new pool instance', async function() {
-    // await deployments.fixture();
-    const { get } = deployments;
-
-    //
-    const POOL_CREATOR_ROLE = await securitizationManagerContract.POOL_CREATOR();
-    await securitizationManagerContract.grantRole(POOL_CREATOR_ROLE, poolCreatorSigner.address);
-    // Create new pool
-    const transaction = await securitizationManagerContract.connect(poolCreatorSigner).newPoolInstance(tokenA.address, '100000')
-    const receipt = await transaction.wait();
-    const [SecuritizationPoolAddress] = receipt.events.find(e => e.event == 'NewPoolCreated').args;
-    expect(SecuritizationPoolAddress).to.be.properAddress;
+    ({ stableCoin } = await setupTest());
   });
   it('should emit RoleGranted event with an address', async function() {
     const POOL_CREATOR_ROLE = await securitizationManagerContract.POOL_CREATOR();
@@ -56,6 +43,31 @@ describe('SecuritizationManager', () => {
     await expect(transaction)
       .to.emit(securitizationManagerContract, "RoleGranted")
       .withArgs(POOL_CREATOR_ROLE, poolCreatorSigner.address, untangledAdminSigner.address);
+
+  });
+  describe('#newPoolInstance', async () => {
+    it('Should create new pool instance', async function() {
+      // await deployments.fixture();
+      const { get } = deployments;
+
+      //
+      const POOL_CREATOR_ROLE = await securitizationManagerContract.POOL_CREATOR();
+      await securitizationManagerContract.grantRole(POOL_CREATOR_ROLE, poolCreatorSigner.address);
+      // Create new pool
+      const transaction = await securitizationManagerContract.connect(poolCreatorSigner).newPoolInstance(stableCoin.address, '100000')
+      const receipt = await transaction.wait();
+      const [SecuritizationPoolAddress] = receipt.events.find(e => e.event == 'NewPoolCreated').args;
+      expect(SecuritizationPoolAddress).to.be.properAddress;
+      const securitizationPoolContract = await ethers.getContractAt('SecuritizationPool', SecuritizationPoolAddress);
+      expect(await securitizationPoolContract.underlyingCurrency()).to.equal(stableCoin.address);
+    });
+
+  });
+  describe('#setUpTGEForSOT', async () => {
+
+  });
+
+  describe('#setUpTGEForJOT', async () => {
 
   });
 });
