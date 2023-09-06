40902a757b069dfd080c1f8db006735b792e3ef1
vjtc0n <vjtc0n12345@gmail.com>
2023-09-07 00:02:27 +0700

    Update SecuritizationPool UT

--- test/SecuritizationPool.js
+++ test/SecuritizationPool.js
@@ -3,6 +3,7 @@ const { deployments } = require('hardhat');
 const _ = require('lodash');
 const dayjs = require('dayjs');
 const { expect } = require('./shared/expect.js');
+const { time } = require('@nomicfoundation/hardhat-network-helpers');
 
 const { BigNumber } = ethers;
 const { parseEther, parseUnits, formatEther, formatBytes32String } = ethers.utils;
@@ -36,6 +37,7 @@ describe('SecuritizationPool', () => {
   let distributionOperator;
   let sotToken;
   let distributionTranche;
+  let mintedIncreasingInterestTGE;
 
   // Wallets
   let untangledAdminSigner, poolCreatorSigner, originatorSigner, borrowerSigner, lenderSigner, relayer;
@@ -90,6 +92,9 @@ describe('SecuritizationPool', () => {
       let [securitizationPoolAddress] = receipt.events.find((e) => e.event == 'NewPoolCreated').args;
 
       securitizationPoolContract = await ethers.getContractAt('SecuritizationPool', securitizationPoolAddress);
+      await securitizationPoolContract
+        .connect(poolCreatorSigner)
+        .grantRole(await securitizationPoolContract.ORIGINATOR_ROLE(), originatorSigner.address);
 
       transaction = await securitizationManager
         .connect(poolCreatorSigner)
@@ -98,6 +103,9 @@ describe('SecuritizationPool', () => {
       [securitizationPoolAddress] = receipt.events.find((e) => e.event == 'NewPoolCreated').args;
 
       secondSecuritizationPool = await ethers.getContractAt('SecuritizationPool', securitizationPoolAddress);
+      await secondSecuritizationPool
+        .connect(poolCreatorSigner)
+        .grantRole(await secondSecuritizationPool.ORIGINATOR_ROLE(), originatorSigner.address);
 
       const oneDayInSecs = 1 * 24 * 3600;
       const halfOfADay = oneDayInSecs / 2;
@@ -136,8 +144,6 @@ describe('SecuritizationPool', () => {
   });
 
   describe('#Securitization Manager', async () => {
-    let mintedIncreasingInterestTGE;
-
     it('Should set up TGE for SOT successfully', async () => {
       const tokenDecimals = 18;
 
@@ -145,8 +151,8 @@ describe('SecuritizationPool', () => {
       const closingTime = dayjs(new Date()).add(7, 'days').unix();
       const rate = 2;
       const totalCapOfToken = parseEther('100000');
-      const initialInterest = 100000;
-      const finalInterest = 100000;
+      const initialInterest = 10000;
+      const finalInterest = 10000;
       const timeInterval = 1 * 24 * 3600; // seconds
       const amountChangeEachInterval = 0;
       const prefixOfNoteTokenSaleName = 'SOT_';
@@ -199,6 +205,8 @@ describe('SecuritizationPool', () => {
       await distributionOperator
         .connect(lenderSigner)
         .makeRedeemRequestAndRedeem(securitizationPoolContract.address, sotToken.address, parseEther('10'));
+
+      expect(formatEther(await sotToken.balanceOf(lenderSigner.address))).equal('90.0');
     });
   });
 
@@ -276,7 +284,9 @@ describe('SecuritizationPool', () => {
         loanKernel.fillDebtOrder(orderAddresses, orderValues, termsContractParameters, tokenIds)
       ).to.be.revertedWith(`ERC721: token already minted`);
     });
+  });
 
+  describe('#Securitization Pool', async () => {
     it('#exportAssets', async () => {
       await securitizationPoolContract
         .connect(poolCreatorSigner)
@@ -289,6 +299,100 @@ describe('SecuritizationPool', () => {
       expect(balanceOfPool).equal(1);
     });
 
+    it('#setPot', async () => {
+      await securitizationPoolContract.connect(poolCreatorSigner).setPot(secondSecuritizationPool.address);
+      expect(await securitizationPoolContract.pot()).equal(secondSecuritizationPool.address);
+
+      // Set again
+      await securitizationPoolContract.connect(poolCreatorSigner).setPot(securitizationPoolContract.address);
+      expect(await securitizationPoolContract.pot()).equal(securitizationPoolContract.address);
+    });
+
+    it('#withdrawAssets', async () => {
+      await secondSecuritizationPool
+        .connect(poolCreatorSigner)
+        .withdrawAssets([loanAssetTokenContract.address], [tokenIds[1]], [originatorSigner.address]);
+
+      const ownerOfAgreement = await loanAssetTokenContract.ownerOf(tokenIds[1]);
+      expect(ownerOfAgreement).equal(originatorSigner.address);
+
+      const balanceOfPoolCreator = await loanAssetTokenContract.balanceOf(originatorSigner.address);
+      expect(balanceOfPoolCreator).equal(1);
+    });
+
+    it('#collectAssets', async () => {
+      await loanAssetTokenContract.connect(originatorSigner).setApprovalForAll(secondSecuritizationPool.address, true);
+
+      await secondSecuritizationPool
+        .connect(originatorSigner)
+        .collectAssets(loanAssetTokenContract.address, originatorSigner.address, [tokenIds[1]]);
+
+      const ownerOfAgreement = await loanAssetTokenContract.ownerOf(tokenIds[1]);
+      expect(ownerOfAgreement).equal(secondSecuritizationPool.address);
+
+      const balanceOfPoolCreator = await loanAssetTokenContract.balanceOf(secondSecuritizationPool.address);
+      expect(balanceOfPoolCreator).equal(1);
+    });
+
+    // it('#withdraw', async () => {
+    //   await securitizationPoolContract.connect(originatorSigner).withdraw(parseEther('10'));
+    // });
+
+    it('#collectERC20Assets', async () => {
+      await sotToken.connect(lenderSigner).approve(securitizationPoolContract.address, unlimitedAllowance);
+
+      await securitizationPoolContract
+        .connect(originatorSigner)
+        .collectERC20Assets([sotToken.address], [lenderSigner.address], [parseEther('2')]);
+
+      expect(formatEther(await sotToken.balanceOf(lenderSigner.address))).equal('88.0');
+    });
+
+    it('#withdrawERC20Assets', async () => {
+      await securitizationPoolContract
+        .connect(poolCreatorSigner)
+        .withdrawERC20Assets([sotToken.address], [lenderSigner.address], [parseEther('1')]);
+
+      expect(formatEther(await sotToken.balanceOf(lenderSigner.address))).equal('89.0');
+    });
+
+    it('#claimERC20Assets', async () => {
+      await securitizationPoolContract.connect(poolCreatorSigner).claimERC20Assets([sotToken.address]);
+
+      expect(formatEther(await sotToken.balanceOf(lenderSigner.address))).equal('89.0');
+    });
+
+    it('#claimCashRemain', async () => {
+      expect(formatEther(await stableCoin.balanceOf(poolCreatorSigner.address))).equal('0.0');
+      await expect(
+        securitizationPoolContract.connect(poolCreatorSigner).claimCashRemain(poolCreatorSigner.address)
+      ).to.be.revertedWith(`SecuritizationPool: SOT still remain`);
+
+      await stableCoin.mint(parseEther('100000000000000'));
+      await stableCoin.connect(untangledAdminSigner).transfer(lenderSigner.address, parseEther('1000000'));
+      await securitizationManager
+        .connect(lenderSigner)
+        .buyTokens(mintedIncreasingInterestTGE.address, parseEther('90000'));
+      await securitizationPoolContract.connect(poolCreatorSigner).claimCashRemain(poolCreatorSigner.address);
+    });
+
+    it('#startCycle', async () => {
+      expect(formatEther(await stableCoin.balanceOf(poolCreatorSigner.address))).equal('0.0');
+      await expect(
+        securitizationPoolContract
+          .connect(poolCreatorSigner)
+          .startCycle(86400, parseEther('10000'), 5000, dayjs(new Date()).add(8, 'days').unix())
+      ).to.be.revertedWith(`SecuritizationPool: sale is still on going`);
+      await time.increaseTo(dayjs(new Date()).add(8, 'days').unix());
+      await mintedIncreasingInterestTGE.finalize(false, untangledAdminSigner.address);
+
+      await securitizationPoolContract
+        .connect(poolCreatorSigner)
+        .startCycle(86400, parseEther('10000'), 5000, dayjs(new Date()).add(8, 'days').unix());
+    });
+  });
+
+  describe('Burn agreement', async () => {
     it('only LoanKernel contract can burn', async () => {
       await loanRepaymentRouter
         .connect(untangledAdminSigner)
