fc0683d220cde04fe0b30cb337a9995957916f00
vuquangthinh <quangthinh.dico@gmail.com>
2023-09-10 13:13:07 +0700

    add ccip role

--- contracts/protocol/ccip/ExampleResolver.sol
+++ contracts/protocol/ccip/ExampleResolver.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.19;
 
+import { AccessControlUpgradeable } from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract ExampleResolver {
+contract ExampleResolver is AccessControlUpgradeable {
 
-    function hello() external {
+    bytes32 public constant CCIP_RECEIVER_ROLE = keccak256("CCIP_RECEIVER_ROLE");
+
+    function __ExampleResolver_init_unchained() {
+        _setupRole(CCIP_RECEIVER_ROLE);
+    }
+
+    function hello() external onlyRole(CCIP_RECEIVER_ROLE) {
         ///
         // check role
         // msgSender == UntangedReceiver
