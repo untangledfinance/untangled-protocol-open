b8bf39777626a1cbd83c89a88236739ffe80652f
vjtc0n <vjtc0n12345@gmail.com>
2023-09-11 17:12:33 +0700

    Transfer message scripts

---
+++ scripts/03-transfer-message.sh
@@ -0,0 +1,9 @@
+npx hardhat transfer-message \
+  --network sepolia \
+  --receiver "0x7048dc43a99aEb1bD4b3Eb9924f6c34E2C4cA85D" \
+  --destchain "mumbai"
+
+#####
+# TxHash: 0x03c9ebf303100e500fd1d134534d6023a53474b26d3dac99417485ad8ad8c3ca
+# https://ccip.chain.link/msg/0x56859070da9e222a3f85a08d4c199cd20a351aa4d51160fffef32eb705964522
+# ~ 22 minutes to be Success
