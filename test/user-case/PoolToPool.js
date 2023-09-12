a2ca331ea9029f8849c5aa288c16a9afe6ff1f2c
tanlm <luuminhtan046@gmail.com>
2023-09-12 20:25:05 +0700

    Update Pool to Pool test

--- test/user-case/PoolToPool.js
+++ test/user-case/PoolToPool.js
@@ -1,13 +1,10 @@
 const { ethers, getChainId } = require('hardhat');
 const { expect } = require('../shared/expect.js');
 const { mainFixture } = require('../shared/fixtures');
-const { BigNumber } = require('ethers');
 const { parseEther, parseUnits, formatEther, formatBytes32String } = ethers.utils;
 const dayjs = require('dayjs');
 const _ = require('lodash');
-const { admin } = require('@openzeppelin/truffle-upgrades');
 const { time, impersonateAccount, stopImpersonatingAccount, setBalance } = require('@nomicfoundation/hardhat-network-helpers');
-const { parse } = require('dotenv');
 const { presignedMintMessage } = require('../shared/uid-helper');
 
 /**
@@ -84,16 +81,27 @@ describe('Pool to Pool', () => {
     const [jotTGEAddress] = setUpTGEJOTReceipt.events.find(e => e.event == 'NewTGECreated').args;
     const mintedNormalTGEContract = await ethers.getContractAt('MintedNormalTGE', jotTGEAddress);
 
+    // Init SOT sale
+    const sotCap = '10000000000000000000';
+    const isLongSaleTGESOT = true;
+    const setUpTGESOTTransaction = await securitizationManagerContract.connect(poolCreatorSigner).setUpTGEForSOT(poolCreatorSigner.address, securitizationPoolContract.address, [0, 2], isLongSaleTGESOT, 10000, 90000, 86400, 10000, {
+      openingTime: now,
+      closingTime: now + 2 * ONE_DAY,
+      rate: 10000,
+      cap: sotCap,
+    }, 'Ticker');
+    const setUpTGESOTReceipt = await setUpTGESOTTransaction.wait();
+    const [sotTGEAddress] = setUpTGESOTReceipt.events.find(e => e.event == 'NewTGECreated').args;
+    const mintedIncreasingInterestTGEContract = await ethers.getContractAt('MintedIncreasingInterestTGE', sotTGEAddress);
+
     // Create investor pool
     const POOL_CREATOR_ROLE = await securitizationManagerContract.POOL_CREATOR();
     await securitizationManagerContract.grantRole(POOL_CREATOR_ROLE, investorPoolCreator.address);
-
     const transaction = await securitizationManagerContract
       .connect(investorPoolCreator)
       .newPoolInstance(stableCoin.address, '100000');
     const receipt = await transaction.wait();
     const [securitizationPoolAddress] = receipt.events.find((e) => e.event == 'NewPoolCreated').args;
-
     const investorSecuritizationPoolContract = await ethers.getContractAt('SecuritizationPool', securitizationPoolAddress);
     await investorSecuritizationPoolContract.connect(investorPoolCreator).setPot(investorPoolPot.address)
 
@@ -103,7 +111,6 @@ describe('Pool to Pool', () => {
     const expiredAt = now + ONE_DAY;
     const nonce = 0;
     const ethRequired = parseEther("0.00083")
-
     const uidMintMessage = presignedMintMessage(investorPoolPot.address, UID_TYPE, expiredAt, uniqueIdentityContract.address, nonce, chainId)
     const signature = await untangledAdminSigner.signMessage(uidMintMessage)
     await uniqueIdentityContract.connect(investorPoolPot).mint(UID_TYPE, expiredAt, signature, { value: ethRequired });
@@ -116,11 +123,25 @@ describe('Pool to Pool', () => {
     await stableCoin.connect(investorPoolPot).approve(mintedNormalTGEContract.address, stableCoinAmountToBuyJOT)
     await securitizationManagerContract.connect(investorPoolPot).buyTokens(mintedNormalTGEContract.address, stableCoinAmountToBuyJOT)
 
+    // Transfer to pool
+    const jotAddress = await securitizationPoolContract.jotToken();
+    const jotContract = await ethers.getContractAt('NoteToken', jotAddress);
+    const jotAmount = await jotContract.balanceOf(investorPoolPot.address);
+    const ORIGINATOR_ROLE = await investorSecuritizationPoolContract.ORIGINATOR_ROLE();
+    await investorSecuritizationPoolContract.connect(investorPoolCreator).grantRole(ORIGINATOR_ROLE, borrowerSigner.address);
+    await jotContract.connect(investorPoolPot).approve(investorSecuritizationPoolContract.address, jotAmount);
+    await investorSecuritizationPoolContract.connect(borrowerSigner).collectERC20Assets(
+      [jotAddress],
+      [investorPoolPot.address],
+      [jotAmount]
+    )
+
     // Check values
     const chainTime = await time.latest();
     const expectAssetValue = await securitizationPoolValueService.getExpectedAssetsValue(investorSecuritizationPoolContract.address, chainTime)
-    console.log(expectAssetValue);
-    console.log(await investorSecuritizationPoolContract.getTokenAssetAddresses())
+    expect(expectAssetValue).equal(stableCoinAmountToBuyJOT)
+    const tokenERC20AssetAddress = await investorSecuritizationPoolContract.tokenAssetAddresses(0);
+    expect(tokenERC20AssetAddress).equal(jotContract.address);
   });
 
 
