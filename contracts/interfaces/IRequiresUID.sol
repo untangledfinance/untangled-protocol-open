c5bd471ef3dae402adea12b9707e88e564da1aa5
vuquangthinh <quangthinh.dico@gmail.com>
2023-09-23 11:42:17 +0700

    fix version

--- contracts/interfaces/IRequiresUID.sol
+++ contracts/interfaces/IRequiresUID.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.13;
+pragma solidity 0.8.19;
 
 interface IRequiresUID {
   function hasAllowedUID(address sender) external view returns (bool);
--- contracts/protocol/ccip/mock/UntangledReceiverV2.sol
+++ contracts/protocol/ccip/mock/UntangledReceiverV2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.19;
 
 import {Client} from '@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol';
 import '@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol';
