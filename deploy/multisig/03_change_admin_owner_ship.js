cf67ec18a970845d5432c64288246531a89f112d
vjtc0n <vjtc0n12345@gmail.com>
2023-09-27 15:47:09 +0700

    Add example to change admin for proxy

--- deploy/multisig/03_change_admin_owner_ship.js
+++ deploy/multisig/03_change_admin_owner_ship.js
@@ -4,9 +4,26 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { readDotFile, deploy, execute, get, save } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const bridgeRouterProxy = await deployments.get('UntangledBridgeRouter');
+  const untangledBridgeRouter = await deployments.get('UntangledBridgeRouter');
+  const proxyAdminDeployment = await deployments.get('DefaultProxyAdmin');
+  const proxyAdmin = await ethers.getContractAt(
+    '@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol:ProxyAdmin',
+    proxyAdminDeployment.address
+  );
 
-  console.log(bridgeRouterProxy);
+  const bridgeRouterAdmin = await proxyAdmin.getProxyAdmin(untangledBridgeRouter.address);
+
+  console.log(bridgeRouterAdmin);
+
+  await deployments.execute(
+    'DefaultProxyAdmin',
+    {
+      from: deployer,
+    },
+    'changeProxyAdmin',
+    untangledBridgeRouter.address,
+    '0x7a35210B5151147f25B2984686222f3BF68a0fdc'
+  );
 };
 
 module.exports.dependencies = [];
