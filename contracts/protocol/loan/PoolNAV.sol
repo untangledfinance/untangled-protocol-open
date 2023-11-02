70026f7c4a308736a1f57089914e158f63a048ae
Luu Minh Tan <tanlm@bravezone.com>
2023-11-02 17:21:22 +0700

    Merge pile in to poolnav

--- contracts/protocol/loan/PoolNAV.sol
+++ contracts/protocol/loan/PoolNAV.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.19;
 import "./auth.sol";
 import {Discounting} from "./discounting.sol";
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+import "./interest.sol";
 
 interface PileLike {
     function setRate(uint256 loan, uint256 rate) external;
@@ -30,6 +31,35 @@ contract PoolNAV is Auth, Discounting, Initializable {
         uint128 maturityDate;
         uint128 risk;
     }
+
+    /// @notice stores all needed information of an interest rate group
+    struct Rate {
+        // total debt of all loans with this rate
+        uint256 pie;
+        // accumlated rate index over time
+        uint256 chi;
+        // interest rate per second
+        uint256 ratePerSecond;
+        // last time the rate was accumulated
+        uint48 lastUpdated;
+        // fixed rate applied to each loan of the group
+        uint256 fixedRate;
+    }
+
+    /// @notice Interest Rate Groups are identified by a `uint` and stored in a mapping
+    mapping(uint256 => Rate) public rates;
+
+    mapping(uint256 => uint256) public pie;
+
+    /// @notice mapping from loan => rate
+    mapping(uint256 => uint256) public loanRates;
+
+    /// Events
+    event IncreaseDebt(uint256 indexed loan, uint256 currencyAmount);
+    event DecreaseDebt(uint256 indexed loan, uint256 currencyAmount);
+    event SetRate(uint256 indexed loan, uint256 rate);
+    event ChangeRate(uint256 indexed loan, uint256 newRate);
+    event File(bytes32 indexed what, uint256 rate, uint256 value);
 
     uint256 public loanCount;
     mapping(uint256 => uint256) public balances;
@@ -157,6 +187,11 @@ contract PoolNAV is Auth, Discounting, Initializable {
     function initialize(address _pool) public initializer {
         wards[_pool] = 1;
         lastNAVUpdate = uniqueDayTimestamp(block.timestamp);
+
+        // pre-definition for loans without interest rates
+        rates[0].chi = ONE;
+        rates[0].ratePerSecond = ONE;
+
         emit Rely(_pool);
     }
 
@@ -275,14 +310,14 @@ contract PoolNAV is Auth, Discounting, Initializable {
             return;
         }
 
-        uint256 debt = safeSub(pile.debt(loan), amount);
+        uint256 _debt = safeSub(this.debt(loan), amount);
         uint256 preFV = futureValue(nftID_);
         // in case of partial repayment, compute the fv of the remaining debt and add to the according fv bucket
         uint256 fv = 0;
         uint256 fvDecrease = preFV;
-        if (debt != 0) {
+        if (_debt != 0) {
             (,, uint256 loanInterestRate,,) = pile.rates(pile.loanRates(loan));
-            fv = calcFutureValue(loanInterestRate, debt, maturityDate_, recoveryRatePD(risk(nftID_)));
+            fv = calcFutureValue(loanInterestRate, _debt, maturityDate_, recoveryRatePD(risk(nftID_)));
             if (preFV >= fv) {
                 fvDecrease = safeSub(preFV, fv);
             } else {
@@ -570,7 +605,7 @@ contract PoolNAV is Auth, Discounting, Initializable {
     /// @notice returns the nftID for the underlying collateral nft
     /// @param loan the loan id
     /// @return nftID_ the nftID of the loan
-    function nftID(uint256 loan) public view returns (bytes32 nftID_) {
+    function nftID(uint256 loan) public pure returns (bytes32 nftID_) {
         return bytes32(loan);
     }
 
@@ -613,5 +648,124 @@ contract PoolNAV is Auth, Discounting, Initializable {
 
         // returns type(uint128).max if no write-off group is valid for this loan
         return lastValidWriteOff;
+    }
+
+    function incDebt(uint256 loan, uint256 currencyAmount) external auth {
+        uint256 rate = loanRates[loan];
+        require(block.timestamp == rates[rate].lastUpdated, "rate-group-not-updated");
+        currencyAmount = safeAdd(currencyAmount, rmul(currencyAmount, rates[rate].fixedRate));
+        uint256 pieAmount = toPie(rates[rate].chi, currencyAmount);
+
+        pie[loan] = safeAdd(pie[loan], pieAmount);
+        rates[rate].pie = safeAdd(rates[rate].pie, pieAmount);
+
+        emit IncreaseDebt(loan, currencyAmount);
+    }
+
+    function decDebt(uint256 loan, uint256 currencyAmount) external auth {
+        uint256 rate = loanRates[loan];
+        require(block.timestamp == rates[rate].lastUpdated, "rate-group-not-updated");
+        uint256 pieAmount = toPie(rates[rate].chi, currencyAmount);
+
+        pie[loan] = safeSub(pie[loan], pieAmount);
+        rates[rate].pie = safeSub(rates[rate].pie, pieAmount);
+
+        emit DecreaseDebt(loan, currencyAmount);
+    }
+
+    function debt(uint256 loan) external view returns (uint256 loanDebt) {
+        uint256 rate_ = loanRates[loan];
+        uint256 chi_ = rates[rate_].chi;
+        if (block.timestamp >= rates[rate_].lastUpdated) {
+            chi_ = chargeInterest(rates[rate_].chi, rates[rate_].ratePerSecond, rates[rate_].lastUpdated);
+        }
+        return toAmount(chi_, pie[loan]);
+    }
+
+    function rateDebt(uint256 rate) external view returns (uint256 totalDebt) {
+        uint256 chi_ = rates[rate].chi;
+        uint256 pie_ = rates[rate].pie;
+
+        if (block.timestamp >= rates[rate].lastUpdated) {
+            chi_ = chargeInterest(rates[rate].chi, rates[rate].ratePerSecond, rates[rate].lastUpdated);
+        }
+        return toAmount(chi_, pie_);
+    }
+
+    function setRate(uint256 loan, uint256 rate) external auth {
+        require(pie[loan] == 0, "non-zero-debt");
+        // rate category has to be initiated
+        require(rates[rate].chi != 0, "rate-group-not-set");
+        loanRates[loan] = rate;
+        emit SetRate(loan, rate);
+    }
+
+    function changeRate(uint256 loan, uint256 newRate) external auth {
+        require(rates[newRate].chi != 0, "rate-group-not-set");
+        uint256 currentRate = loanRates[loan];
+        drip(currentRate);
+        drip(newRate);
+        uint256 pie_ = pie[loan];
+        uint256 debt_ = toAmount(rates[currentRate].chi, pie_);
+        rates[currentRate].pie = safeSub(rates[currentRate].pie, pie_);
+        pie[loan] = toPie(rates[newRate].chi, debt_);
+        rates[newRate].pie = safeAdd(rates[newRate].pie, pie[loan]);
+        loanRates[loan] = newRate;
+        emit ChangeRate(loan, newRate);
+    }
+
+    function accrue(uint256 loan) external {
+        drip(loanRates[loan]);
+    }
+
+    function drip(uint256 rate) public {
+        if (block.timestamp >= rates[rate].lastUpdated) {
+            (uint256 chi,) =
+                            compounding(rates[rate].chi, rates[rate].ratePerSecond, rates[rate].lastUpdated, rates[rate].pie);
+            rates[rate].chi = chi;
+            rates[rate].lastUpdated = uint48(block.timestamp);
+        }
+    }
+
+    /// Interest functions
+    // @notice This function provides compounding in seconds
+    // @param chi Accumulated interest rate over time
+    // @param ratePerSecond Interest rate accumulation per second in RAD(10ˆ27)
+    // @param lastUpdated When the interest rate was last updated
+    // @param pie Total sum of all amounts accumulating under one interest rate, divided by that rate
+    // @return The new accumulated rate, as well as the difference between the debt calculated with the old and new accumulated rates.
+    function compounding(uint chi, uint ratePerSecond, uint lastUpdated, uint pie) public view returns (uint, uint) {
+        require(block.timestamp >= lastUpdated, "tinlake-math/invalid-timestamp");
+        require(chi != 0);
+        // instead of a interestBearingAmount we use a accumulated interest rate index (chi)
+        uint updatedChi = _chargeInterest(chi ,ratePerSecond, lastUpdated, block.timestamp);
+        return (updatedChi, safeSub(rmul(updatedChi, pie), rmul(chi, pie)));
+    }
+
+    // @notice This function charge interest on a interestBearingAmount
+    // @param interestBearingAmount is the interest bearing amount
+    // @param ratePerSecond Interest rate accumulation per second in RAD(10ˆ27)
+    // @param lastUpdated last time the interest has been charged
+    // @return interestBearingAmount + interest
+    function chargeInterest(uint interestBearingAmount, uint ratePerSecond, uint lastUpdated) public view returns (uint) {
+        if (block.timestamp >= lastUpdated) {
+            interestBearingAmount = _chargeInterest(interestBearingAmount, ratePerSecond, lastUpdated, block.timestamp);
+        }
+        return interestBearingAmount;
+    }
+
+    function _chargeInterest(uint interestBearingAmount, uint ratePerSecond, uint lastUpdated, uint current) internal pure returns (uint) {
+        return rmul(rpow(ratePerSecond, current - lastUpdated, ONE), interestBearingAmount);
+    }
+
+
+    // convert pie to debt/savings amount
+    function toAmount(uint chi, uint pie) public pure returns (uint) {
+        return rmul(pie, chi);
+    }
+
+    // convert debt/savings amount to pie
+    function toPie(uint chi, uint amount) public pure returns (uint) {
+        return rdivup(amount, chi);
     }
 }
