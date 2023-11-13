a0e918a7ec60c572c3291035fd84015e6a239088
vuquangthinh <quangthinh.dico@gmail.com>
2023-11-13 14:18:29 +0700

    add validatorRequired as param

--- test/DistributionOperator.js
+++ test/DistributionOperator.js
@@ -100,7 +100,34 @@ describe('Distribution', () => {
       // Create new pool
       let transaction = await securitizationManager
         .connect(poolCreatorSigner)
-        .newPoolInstance(stableCoin.address, '100000', poolCreatorSigner.address, utils.keccak256(Date.now()));
+        .newPoolInstance(
+          utils.keccak256(Date.now()),
+          poolCreatorSigner.address,
+          utils.defaultAbiCoder.encode([
+            {
+              type: 'tuple',
+              components: [
+                {
+                  name: 'currency',
+                  type: 'address'
+                },
+                {
+                  name: 'minFirstLossCushion',
+                  type: 'uint32'
+                },
+                {
+                  name: "validatorRequired",
+                  type: "bool"
+                }
+              ]
+            }
+          ], [
+            {
+              currency: stableCoin.address,
+              minFirstLossCushion: '100000',
+              validatorRequired: true
+            }
+          ]));
 
       let receipt = await transaction.wait();
       let [securitizationPoolAddress] = receipt.events.find((e) => e.event == 'NewPoolCreated').args;
@@ -112,7 +139,35 @@ describe('Distribution', () => {
 
       transaction = await securitizationManager
         .connect(poolCreatorSigner)
-        .newPoolInstance(stableCoin.address, '100000', poolCreatorSigner.address, utils.keccak256(Date.now()));
+        .newPoolInstance(
+          utils.keccak256(Date.now()),
+
+          poolCreatorSigner.address,
+          utils.defaultAbiCoder.encode([
+            {
+              type: 'tuple',
+              components: [
+                {
+                  name: 'currency',
+                  type: 'address'
+                },
+                {
+                  name: 'minFirstLossCushion',
+                  type: 'uint32'
+                },
+                {
+                  name: "validatorRequired",
+                  type: "bool"
+                }
+              ]
+            }
+          ], [
+            {
+              currency: stableCoin.address,
+              minFirstLossCushion: '100000',
+              validatorRequired: true
+            }
+          ]));
       receipt = await transaction.wait();
       [securitizationPoolAddress] = receipt.events.find((e) => e.event == 'NewPoolCreated').args;
 
