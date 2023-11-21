a5c8f9596106f859aa1f0495cca96753ea088a15
vjtc0n <vjtc0n12345@gmail.com>
2023-11-21 15:16:04 +0700

    Update LoanKernel

--- deploy/v4/04_deploy_new_securitization_pool_impl.js
+++ deploy/v4/04_deploy_new_securitization_pool_impl.js
@@ -1,3 +1,5 @@
+const { registrySet } = require('../v2/utils');
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { readDotFile, deploy, execute, get, save } = deployments;
     const { deployer } = await getNamedAccounts();
@@ -7,6 +9,10 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         args: [],
         log: true,
     });
+
+    const contracts = ['SecuritizationPool'];
+
+    await registrySet(contracts);
 };
 
 module.exports.dependencies = [];
