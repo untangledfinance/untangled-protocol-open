5db236f878c4420105d03c7d742bd2df3e27392d
vjtc0n <vjtc0n12345@gmail.com>
2023-11-24 23:44:50 +0700

    Upgrade loan registry

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,111 +15,111 @@ const PRIVATEKEY = process.env.PRIVATEKEY;
 
 const accounts = [PRIVATEKEY];
 module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: '0.8.19',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.19',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
+        overrides: {
+            'contracts/protocol/pool/SecuritizationPool.sol': {
+                version: '0.8.19',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
         },
-      },
-    ],
-    overrides: {
-      'contracts/protocol/pool/SecuritizationPool.sol': {
-        version: '0.8.19',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
     },
-  },
-  defaultNetwork: 'hardhat',
-  namedAccounts: {
-    deployer: 0,
-    invoiceOperator: '0x5380e40aFAd8Cdec0B841c4740985F1735Aa5aCB',
-  },
-  networks: {
-    hardhat: {
-      blockGasLimit: 12500000,
-      saveDeployments: true,
-      allowUnlimitedContractSize: false,
-      accounts: {
-        mnemonic: MNEMONIC,
-      },
-      kycAdmin: '0x9C469Ff6d548D0219575AAc9c26Ac041314AE2bA',
+    defaultNetwork: 'hardhat',
+    namedAccounts: {
+        deployer: 0,
+        invoiceOperator: '0x5380e40aFAd8Cdec0B841c4740985F1735Aa5aCB',
+    },
+    networks: {
+        hardhat: {
+            blockGasLimit: 12500000,
+            saveDeployments: true,
+            allowUnlimitedContractSize: false,
+            accounts: {
+                mnemonic: MNEMONIC,
+            },
+            kycAdmin: '0x9C469Ff6d548D0219575AAc9c26Ac041314AE2bA',
 
-      // forking: {
-      //   url: 'https://alfajores-forno.celo-testnet.org',
-      //   blockNumber: 20237109,
-      // },
-      // chainId: 44787,
-    },
-    celo: {
-      saveDeployments: true,
-      accounts,
-      loggingEnabled: true,
-      url: `https://forno.celo.org`,
-      cusdToken: '0x765DE816845861e75A25fCA122bb6898B8B1282a',
-      usdcToken: '0xef4229c8c3250c675f21bcefa42f58efbff6002a',
-      kycAdmin: '0x9C469Ff6d548D0219575AAc9c26Ac041314AE2bA',
-    },
-    alfajores: {
-      saveDeployments: true,
-      accounts: accounts,
-      loggingEnabled: true,
-      url: `https://alfajores-forno.celo-testnet.org`,
-      cusdToken: '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1',
-      usdcToken: '',
-      kycAdmin: '0x9C469Ff6d548D0219575AAc9c26Ac041314AE2bA',
-    },
-    alfajores_v2: {
-      saveDeployments: true,
-      accounts: accounts,
-      loggingEnabled: true,
-      url: `https://alfajores-forno.celo-testnet.org`,
-      cusdToken: '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1',
-      usdcToken: '',
-      kycAdmin: '0x9C469Ff6d548D0219575AAc9c26Ac041314AE2bA',
-    },
-    rinkeby: {
-      saveDeployments: true,
-      accounts,
-      loggingEnabled: true,
-      url: `https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`,
-    },
-    ...networks,
-  },
-  etherscan: {
-    apiKey: '',
-    customChains: [
-      {
-        network: 'alfajores',
-        chainId: 44787,
-        urls: {
-          apiURL: 'https://api-alfajores.celoscan.io/api',
-          browserURL: 'https://api-alfajores.celoscan.io',
+            // forking: {
+            //     url: 'https://alfajores-forno.celo-testnet.org',
+            //     blockNumber: 21091279,
+            // },
+            // chainId: 44787,
         },
-      },
-    ],
-  },
-  mocha: {
-    timeout: 200000,
-  },
-  paths: {
-    sources: './contracts',
-    tests: './test',
-    artifacts: './artifacts',
-    cache: './cache',
-  },
-  contractSizer: {
-    alphaSort: true,
-    runOnCompile: true,
-    disambiguatePaths: false,
-  },
+        celo: {
+            saveDeployments: true,
+            accounts,
+            loggingEnabled: true,
+            url: `https://forno.celo.org`,
+            cusdToken: '0x765DE816845861e75A25fCA122bb6898B8B1282a',
+            usdcToken: '0xef4229c8c3250c675f21bcefa42f58efbff6002a',
+            kycAdmin: '0x9C469Ff6d548D0219575AAc9c26Ac041314AE2bA',
+        },
+        alfajores: {
+            saveDeployments: true,
+            accounts: accounts,
+            loggingEnabled: true,
+            url: `https://alfajores-forno.celo-testnet.org`,
+            cusdToken: '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1',
+            usdcToken: '',
+            kycAdmin: '0x9C469Ff6d548D0219575AAc9c26Ac041314AE2bA',
+        },
+        alfajores_v2: {
+            saveDeployments: true,
+            accounts: accounts,
+            loggingEnabled: true,
+            url: `https://alfajores-forno.celo-testnet.org`,
+            cusdToken: '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1',
+            usdcToken: '',
+            kycAdmin: '0x9C469Ff6d548D0219575AAc9c26Ac041314AE2bA',
+        },
+        rinkeby: {
+            saveDeployments: true,
+            accounts,
+            loggingEnabled: true,
+            url: `https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`,
+        },
+        ...networks,
+    },
+    etherscan: {
+        apiKey: '',
+        customChains: [
+            {
+                network: 'alfajores',
+                chainId: 44787,
+                urls: {
+                    apiURL: 'https://api-alfajores.celoscan.io/api',
+                    browserURL: 'https://api-alfajores.celoscan.io',
+                },
+            },
+        ],
+    },
+    mocha: {
+        timeout: 200000,
+    },
+    paths: {
+        sources: './contracts',
+        tests: './test',
+        artifacts: './artifacts',
+        cache: './cache',
+    },
+    contractSizer: {
+        alphaSort: true,
+        runOnCompile: true,
+        disambiguatePaths: false,
+    },
 };
