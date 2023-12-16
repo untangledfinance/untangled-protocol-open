6554089c793c6dbee58edbaf7774f3bff9d09e5c
vuquangthinh <quangthinh.dico@gmail.com>
2023-12-16 18:57:07 +0700

    update deploy script

--- tasks/index.js
+++ tasks/index.js
@@ -3,3 +3,6 @@ exports.setupReceiver = require('./02_setupReceiver.js');
 exports.transferMessage = require('./03_transferMessage.js');
 exports.getInfoFromReceiver = require('./04_getInfoFromReceiver.js');
 exports.validateContracts = require('./contracts-validation.js');
+
+require('./deploy-securitization-pool-template.js');
+require('./upgrade-securitization-pool.js');
