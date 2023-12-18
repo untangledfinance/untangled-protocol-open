ce09e77e63d3c1938cd2775f4d2d82c3619a45bd
vjtc0n <vjtc0n12345@gmail.com>
2023-12-18 11:09:38 +0700

    Add deployments

--- deploy/010-distribution-accessor.js
+++ deploy/010-distribution-accessor.js
@@ -1,24 +1,23 @@
-
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy, execute, get } = deployments;
-  const { deployer } = await getNamedAccounts();
+    const { deploy, execute, get } = deployments;
+    const { deployer } = await getNamedAccounts();
 
-  const registry = await deployments.get('Registry');
+    const registry = await deployments.get('Registry');
 
-  const deployResult = await deployments.deploy('DistributionAssessor', {
-    from: deployer,
-    proxy: {
-      proxyContract: 'OpenZeppelinTransparentProxy',
-      execute: {
-        methodName: 'initialize',
-        args: [registry.address],
-      },
-    },
-    skipIfAlreadyDeployed: true,
-    log: true,
-  });
+    const deployResult = await deployments.deploy('DistributionAssessor', {
+        from: deployer,
+        proxy: {
+            proxyContract: 'OpenZeppelinTransparentProxy',
+            execute: {
+                methodName: 'initialize',
+                args: [registry.address],
+            },
+        },
+        skipIfAlreadyDeployed: true,
+        log: true,
+    });
 
-  await execute('Registry', { from: deployer, log: true }, 'setDistributionAssessor', deployResult.address);
+    await execute('Registry', { from: deployer, log: true }, 'setDistributionAssessor', deployResult.address);
 };
 
 module.exports.dependencies = ['registry'];
