061a198460585de16d350cad862ccd267140f227
vjtc0n <vjtc0n12345@gmail.com>
2023-12-31 10:48:21 +0700

    Temporarily remove CCIP code

--- contracts/protocol/ccip/interfaces/IUntangledBridgeRouter.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-
-pragma solidity 0.8.19;
-
-import {MessageType} from '../ICommandData.sol';
-
-interface IUntangledBridgeRouter {
-    function processMessage(MessageType messageType, bytes calldata data) external;
-}
